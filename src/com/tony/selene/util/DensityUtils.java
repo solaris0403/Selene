373d45cede8909e3f2c4d8f3b0412e1480745597
tony.cao <tonycao@augmentum.com.cn>
2015-07-29 18:09:15 +0800

    daily sync

--- src/com/tony/selene/util/DensityUtils.java
+++ src/com/tony/selene/util/DensityUtils.java
@@ -1,62 +1,52 @@
 ﻿package com.tony.selene.util;
 
-
-import java.text.DecimalFormat;
-
 import android.content.Context;
 import android.util.TypedValue;
 
 //常用单位转换的辅助类
-public class DensityUtils
-{
+public class DensityUtils {
 	/**
-	 * dp转px
+	 * dpTopx
 	 * 
 	 * @param context
 	 * @param val
 	 * @return
 	 */
-	public static int dp2px(Context context, float dpVal)
-	{
-		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-				dpVal, context.getResources().getDisplayMetrics());
+	public static int dp2px(Context context, float dpVal) {
+		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpVal, context.getResources().getDisplayMetrics());
 	}
 
 	/**
-	 * sp转px
+	 * spTopx
 	 * 
 	 * @param context
 	 * @param val
 	 * @return
 	 */
-	public static int sp2px(Context context, float spVal)
-	{
-		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
-				spVal, context.getResources().getDisplayMetrics());
+	public static int sp2px(Context context, float spVal) {
+		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spVal, context.getResources().getDisplayMetrics());
 	}
-	
+
 	/**
-	 * px转dp
+	 * pxTodp
 	 * 
 	 * @param context
 	 * @param pxVal
 	 * @return
 	 */
-	public static float px2dp(Context context, float pxVal)
-	{
+	public static float px2dp(Context context, float pxVal) {
 		final float scale = context.getResources().getDisplayMetrics().density;
 		return (pxVal / scale);
 	}
 
 	/**
-	 * px转sp
+	 * pxTosp
 	 * 
 	 * @param fontScale
 	 * @param pxVal
 	 * @return
 	 */
-	public static float px2sp(Context context, float pxVal)
-	{
+	public static float px2sp(Context context, float pxVal) {
 		return (pxVal / context.getResources().getDisplayMetrics().scaledDensity);
-	}	
+	}
 }
