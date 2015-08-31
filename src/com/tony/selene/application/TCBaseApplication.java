612440a64e1d53d66aa20e4a395036ce9e396bc6
tony.cao <tonycao@augmentum.com.cn>
2015-08-31 09:40:36 +0800

    finish application

--- src/com/tony/selene/application/TCBaseApplication.java
+++ src/com/tony/selene/application/TCBaseApplication.java
@@ -5,15 +5,19 @@ import android.app.Application;
 import com.tony.selene.activity.TCActivityManager;
 
 /**
- * @author Tony E-mail:solaris0403@gmail.com
- * @version Create Data：Aug 28, 2015 9:18:19 AM
+ * @ClassName TCBaseApplication
+ * @Description Application的父类 实现通用方法
+ * @author Tony.Cao E-mail:solaris0403@gmail.com
+ * @date Aug 31, 2015
  */
-public class TCBaseApplication extends Application{
-    /**
-     * 退出应用程序的时候，手动调用,结束所有的Activity
-     */
-    @Override
-    public void onTerminate() {
-    	TCActivityManager.getInstance().clearAllActivity();
-    }
+public class TCBaseApplication extends Application {
+	
+	/**
+	 * 退出应用程序的时候，手动调用,结束所有的Activity
+	 * @see android.app.Application#onTerminate()
+	 */
+	@Override
+	public void onTerminate() {
+		TCActivityManager.getInstance().clearAllActivity();
+	}
 }
